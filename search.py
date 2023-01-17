9b70bad35cb6f1ba2319979858d49049e5112f7a
rahulgaur03 <rahulgaur0304@gmail.com>
2023-01-17 23:48:16 +0530

    Add voice recognizer

--- search.py
+++ search.py
@@ -1,9 +1,21 @@
 import requests
 import pyttsx3
- 
+import speech_recognition as sr
+
+listener = sr.Recognizer()
 engine = pyttsx3.init()
-text = "How to learn ML"
-result = requests.get(f"http://127.0.0.1:5001/chat?q={text}")
-print(result.content)
-engine.say(result.content.decode('utf-8'))
-engine.runAndWait()
+
+while True:
+    text = ""
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        text = listener.recognize_google(voice)
+        print(text)
+
+    result = requests.get(f"http://127.0.0.1:5001/chat?q={text}")
+    print(result.content)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.say(result.content.decode('utf-8'))
+    engine.runAndWait()
